d4be77a1687762a1f9456b4ca94ef861f7fe90b7
Mads Gadeberg <mads.gadeberg1995@gmail.com>
2018-10-11 14:39:53 +0200

    a lot of stuff

--- project/js/speakers11.js
+++ project/js/speakers11.js
@@ -1,14 +1,18 @@
-$.ajax({
-    url: 'http://speakershopapp.azurewebsites.net/api/speakers',
-    type: 'GET',
-    dataType: 'json',
-    success: function (speakers) {
-        onGetSpeakersSuccess(speakers);
-    },
-    error: function (request, message, error) {
-        handleException(request, message, error);
-    }
-});
+var mySpeakerId = 0;
+
+function getSide1() {
+    $.ajax({
+        url: 'https://speakershopapp.azurewebsites.net/api/speakers?CurrentPage=1&ItemsPrPage=8',
+        type: 'GET',
+        dataType: 'json',
+        success: function (speakers) {
+            onGetSpeakersSuccess(speakers);
+        },
+        error: function (request, message, error) {
+            handleException(request, message, error);
+        }
+    });
+}
 
 
 
@@ -25,7 +29,7 @@ function onGetSpeakersSuccess(speakers) {
             "                        <div id=\"product_title\">\n" +
             "                                <h3>"+speaker.speakerName+"</h3>\n" +
             "                            </div>\n" +
-            "                        <div id=\"product_img\">\n" +
+            "                        <div class=\"product_img\">\n" +
             "                           <img src=" + speaker.url +" alt=\"\">\n" +
             "                        </div>\n" +
             "                        \n" +
@@ -43,21 +47,25 @@ function onGetSpeakersSuccess(speakers) {
             "                            </a>\n" +
             "                            </div>\n" +
             "                            <div id=\"product_info\">\n" +
-            "                                    <a href=\"viewproduct.html\" onclick='getSpeakersById(speakerId)'>MORE INFO</a>\n" +
+            "                                    <button onclick=storeSpeakerAndOpenUrl("+ speaker.speakerId +",'viewproduct.html');>MORE INFO</button>\n" +
             "                            </div>\n" +
             "                        </div>\n" +
             "                </div>")
-
     });
 }
+function storeSpeakerAndOpenUrl(id, url){
+    mySpeakerId = id;
+    window.open(url,"_self");
+}
 
-function getSpeakersById(id) {
-    $.ajax({
-        url: 'http://speakershopapp.azurewebsites.net/api/speakers/' + id,
+
+function getSpeakersById() {
+$.ajax({
+        url: 'https://speakershopapp.azurewebsites.net/api/speakers/' + mySpeakerId,
         type: 'GET',
         dataType: 'json',
-        success: function (speaker) {
-            onGetSpeakersByIdSuccess(speaker);
+        success: function (speakers) {
+            onGetSpeakersByIdSuccess(speakers);
         },
         error: function (request, message, error) {
             handleException(request, message, error);
@@ -66,6 +74,19 @@ function getSpeakersById(id) {
 }
 
 function onGetSpeakersByIdSuccess(speaker) {
-
+    $("#product_view").html("");
 }
 
+function getSide2(){
+    $.ajax({
+        url: 'https://speakershopapp.azurewebsites.net/api/speakers?CurrentPage=2&ItemsPrPage=8',
+        type: 'GET',
+        dataType: 'json',
+        success: function (speakers) {
+            onGetSpeakersSuccess(speakers);
+        },
+        error: function (request, message, error) {
+            handleException(request, message, error);
+        }
+    });
+}
